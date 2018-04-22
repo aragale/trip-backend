af9eebe83e462aa7a84cddc74918953a0965157a
yuanzhen <zenuor@gmail.com>
2018-04-22 15:04:30 +0800

    删除注解

--- trip/data.py
+++ trip/data.py
@@ -7,7 +7,7 @@ from .models import BASE as __BASE
 __ENGINE = create_engine('sqlite:///trip.db', echo=True)
 # 若不存在表，则创建表
 __BASE.metadata.create_all(__ENGINE)
-__SESSION_MAKER: sessionmaker = sessionmaker(bind=__ENGINE, expire_on_commit=False)
+__SESSION_MAKER = sessionmaker(bind=__ENGINE, expire_on_commit=False)
 
 
 def get_session() -> session:
